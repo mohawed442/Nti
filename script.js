3461e8ce455fc7af7c9452687a4fe65390bf5797
unknown <mohamed@442.com>
2025-06-21 14:05:37 +0300

    Day 4: Getting Started with Node.js

--- script.js
+++ script.js
@@ -1,11 +1,84 @@
-console.log("hello");
-function f() {
-    let start = Date.now() ;
-    
-    while (Date.now() - start < 3000) {} ;
-    console.log(Date.now() );
-    console.log(start );
-    console.log(Date.now() - start);
+// console.log("hello");
+// function f() {
+//     let start = Date.now();
+//     while (Date.now() - start < 3000) {}
+//         console.log(Date.now());
+//         console.log(start);
+//         console.log(Date.now() - start);
+// }
 
-}
-f()
+// f();
+
+
+
+
+// node js 
+// let chalk = require('chalk')
+// import chalk from "chalk";  
+// let he = "hello"
+// console.log(chalk.green("hello") );
+
+
+// const os = require('os')
+// console.log(os.platform);
+// console.log(os.homedir);
+
+
+
+
+
+// const fs = require("fs")
+
+
+// fs.readFile("./hello.txt","utf-8",(err , data)=>{
+//     if (err) {
+//         console.log(`error ${err}`);
+//     }
+//     console.log(data);
+    
+// })
+
+
+// let myData = fs.readFileSync("./hello.txt","utf-8") ;
+// console.log(myData);
+
+
+// fs.writeFile("./hello2.txt","this me", (ele)=>console.log(ele))
+
+
+
+
+
+
+
+//  Basic HTTP Server
+const student = {
+    name: 'John Doe',
+    age: 21
+    };
+const http = require("http") ;
+let server = http.createServer((req , res)=>{
+    console.log("A user connected to the server");
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(student)); // Convert object to JSON and send
+});
+
+server.on("request",(req , res)=>{
+    if (req.url === "/about") {
+        res.end("hello ")
+    }
+
+    console.log(req.url);
+    console.log(req.headers);
+    
+    res.statusCode = 200; // Status code: OK
+    res.write('Hello, world!');
+    res.end()
+});
+
+
+server.listen(3000,()=>{
+    console.log('Server is running on http://localhost:3000');
+}) ;
+
+
---
+++ task.js
@@ -0,0 +1,39 @@
+// // task one 
+let figlet = require("figlet");
+let fs = require("fs");
+let fo ;
+fs.writeFile("./hello.txt","hello world", (ele)=>console.log(ele)) ;
+fs.readFile("./hello.txt","utf-8",(err , data)=>{
+fo = data ;
+
+figlet(fo, function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+})
+})
+
+
+// task tow
+const http = require("http") ;
+
+let server = http.createServer((req , res ) =>{
+    if (req.url === "/") {
+        res.end('Welcome to the Homepage!')
+    }else if(req.url === "/about"){
+        res.end("This is the About Page.")
+    }else if(req.url === "/contact"){
+        res.end("Contact us at contact@example.com")
+    }else{
+        res.end("404 - Page Not Found")
+    }
+})
+
+
+
+server.listen(3000,()=>{
+    console.log('Server is running on http://localhost:3000');
+}) ;
